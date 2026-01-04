f48dc951e3883d49eb30a3a2000c5e3b37331e7f
Paweł Kossakowski <pawel.kossakowski2.stud@pw.edu.pl>
2026-01-04 01:01:25 +0100

    Added config.json, modified gui, chart generation now works, saving to text file works also.

--- gui.py
+++ gui.py
@@ -1,17 +1,23 @@
 from PySide6.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
+import matplotlib.pyplot as plt
 from ui_interface import Ui_MainWindow
 from classes import MeshSession
 import sys
 import datetime
+from time import sleep
+import json
+import os
 
 class MeshDescriptorsWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self._descriptors = []
+        self._config = json.load(open("config.json"))
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.searchButton.clicked.connect(self._handleSearchClicked)
         self.ui.saveButton.clicked.connect(self._handleSaveClicked)
+        self.ui.chartButton.clicked.connect(self._handleGenerateChartClicked)
 
     @property
     def descriptors(self) -> list:
@@ -20,14 +26,17 @@ class MeshDescriptorsWindow(QMainWindow):
     @descriptors.setter
     def descriptors(self, value: list):
         self._descriptors = value
+    
+    @property
+    def config(self) -> dict:
+        return self._config
 
     def _handleSearchClicked(self):
+        self.ui.statusbar.showMessage("Searching for descriptors...")
         session = MeshSession(self.ui.medicalText.text())
         self.descriptors = session.get_descriptors()
         if len(self.descriptors) == 0:
-            self.ui.resultTable.setRowCount(1)
-            self.ui.resultTable.setItem(0, 0, QTableWidgetItem("No descriptors found"))
-            self.ui.resultTable.setItem(0, 1, QTableWidgetItem(""))
+            self.ui.statusbar.showMessage("No descriptors found")
         else:
             self.ui.resultTable.setRowCount(len(self.descriptors))
             row = 0
@@ -35,18 +44,46 @@ class MeshDescriptorsWindow(QMainWindow):
                 self.ui.resultTable.setItem(row, 0, QTableWidgetItem(descriptor))
                 self.ui.resultTable.setItem(row, 1, QTableWidgetItem(str(count)))
                 row += 1
+            self.ui.statusbar.showMessage(f"Found {len(self.descriptors)} descriptors")
     
     def _handleSaveClicked(self):
+        self.ui.statusbar.showMessage("Saving descriptors...")
         if self.descriptors:
-            file_name = f"mesh_descriptors{datetime.datetime.now().strftime("%d%m%Y%H%M%S")}.txt"
+            file_folder = self.config["text_file_save_path"]
+            if not file_folder.endswith('/'):
+                file_folder += '/'
+            if not os.path.exists(file_folder):
+                os.makedirs(file_folder)
+            file_name = f"{file_folder}mesh_descriptors{datetime.datetime.now().strftime("%d%m%Y%H%M%S")}.txt"
             with open(file_name, "w") as f:
                 for descriptor, count in self.descriptors:
                     f.write(f"{descriptor}: {count}\n")
-            self.ui.resultTable.setRowCount(1)
-            self.ui.resultTable.setItem(0, 0, QTableWidgetItem(f"Descriptors saved successfully as {file_name}"))
+            self.ui.statusbar.showMessage(f"Descriptors saved successfully as {file_name}")
         else:
-            self.ui.resultTable.setRowCount(1)
-            self.ui.resultTable.setItem(0, 0, QTableWidgetItem("No descriptors to save"))
+            self.ui.statusbar.showMessage("No descriptors to save")
+
+    
+    def _handleGenerateChartClicked(self):
+        self.ui.statusbar.showMessage("Generating chart...")
+        if self.descriptors:
+            fig, ax = plt.subplots()
+            descriptors, counts = zip(*self.descriptors)
+            ax.bar(descriptors, counts)
+            ax.set_ylabel("Count")
+            ax.set_title("MeSH Descriptors Frequency")
+            plt.xticks(rotation=45, ha='right')
+            chart_folder = self.config["chart_save_path"]
+            if not chart_folder.endswith('/'):
+                chart_folder += '/'
+            if not os.path.exists(chart_folder):
+                os.makedirs(chart_folder)
+            chart_file = f"mesh_descriptors_chart{datetime.datetime.now().strftime('%d%m%Y%H%M%S')}.png"
+            chart_path = os.path.join(chart_folder, chart_file)
+            plt.savefig(chart_path, format='png')
+            sleep(0.1)
+            self.ui.statusbar.showMessage(f"Chart saved successfully as {chart_file}")
+            plt.show()
+
 
 
 def guiMain(args):
--- ui_interface.py
+++ ui_interface.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'interfaceKRFdJO.ui'
+## Form generated from reading UI file 'interfaceoUYqRP.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.5.0
+## Created by: Qt User Interface Compiler version 6.10.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -24,19 +24,54 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.setWindowModality(Qt.NonModal)
+        MainWindow.setWindowModality(Qt.WindowModality.NonModal)
         MainWindow.resize(1122, 851)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.centralwidget.setCursor(QCursor(Qt.ArrowCursor))
+        self.centralwidget.setCursor(QCursor(Qt.CursorShape.ArrowCursor))
         self.gridLayout = QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName(u"gridLayout")
         self.gridLayout.setVerticalSpacing(9)
         self.gridLayout.setContentsMargins(9, -1, -1, -1)
+        self.resultTable = QTableWidget(self.centralwidget)
+        if (self.resultTable.columnCount() < 2):
+            self.resultTable.setColumnCount(2)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.resultTable.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.resultTable.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        self.resultTable.setObjectName(u"resultTable")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.resultTable.sizePolicy().hasHeightForWidth())
+        self.resultTable.setSizePolicy(sizePolicy)
+        self.resultTable.setMinimumSize(QSize(0, 0))
+        self.resultTable.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.resultTable.setAutoFillBackground(False)
+        self.resultTable.setFrameShadow(QFrame.Shadow.Plain)
+        self.resultTable.setLineWidth(1)
+        self.resultTable.setSelectionMode(QAbstractItemView.SelectionMode.NoSelection)
+        self.resultTable.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectItems)
+        self.resultTable.setTextElideMode(Qt.TextElideMode.ElideLeft)
+        self.resultTable.setShowGrid(True)
+        self.resultTable.setGridStyle(Qt.PenStyle.NoPen)
+        self.resultTable.setWordWrap(True)
+        self.resultTable.setColumnCount(2)
+        self.resultTable.horizontalHeader().setVisible(True)
+        self.resultTable.horizontalHeader().setCascadingSectionResizes(True)
+        self.resultTable.horizontalHeader().setMinimumSectionSize(33)
+        self.resultTable.horizontalHeader().setDefaultSectionSize(300)
+        self.resultTable.verticalHeader().setVisible(True)
+        self.resultTable.verticalHeader().setMinimumSectionSize(20)
+        self.resultTable.verticalHeader().setDefaultSectionSize(30)
+
+        self.gridLayout.addWidget(self.resultTable, 1, 0, 1, 1)
+
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setSpacing(5)
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.horizontalLayout_2.setSizeConstraint(QLayout.SetDefaultConstraint)
+        self.horizontalLayout_2.setSizeConstraint(QLayout.SizeConstraint.SetDefaultConstraint)
         self.horizontalLayout_2.setContentsMargins(0, -1, -1, -1)
         self.medicalText = QLineEdit(self.centralwidget)
         self.medicalText.setObjectName(u"medicalText")
@@ -45,11 +80,11 @@ class Ui_MainWindow(object):
 
         self.searchButton = QPushButton(self.centralwidget)
         self.searchButton.setObjectName(u"searchButton")
-        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.searchButton.sizePolicy().hasHeightForWidth())
-        self.searchButton.setSizePolicy(sizePolicy)
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.searchButton.sizePolicy().hasHeightForWidth())
+        self.searchButton.setSizePolicy(sizePolicy1)
 
         self.horizontalLayout_2.addWidget(self.searchButton)
 
@@ -71,41 +106,6 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addLayout(self.horizontalLayout, 2, 0, 1, 1)
 
-        self.resultTable = QTableWidget(self.centralwidget)
-        if (self.resultTable.columnCount() < 2):
-            self.resultTable.setColumnCount(2)
-        __qtablewidgetitem = QTableWidgetItem()
-        self.resultTable.setHorizontalHeaderItem(0, __qtablewidgetitem)
-        __qtablewidgetitem1 = QTableWidgetItem()
-        self.resultTable.setHorizontalHeaderItem(1, __qtablewidgetitem1)
-        self.resultTable.setObjectName(u"resultTable")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        sizePolicy1.setHorizontalStretch(0)
-        sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.resultTable.sizePolicy().hasHeightForWidth())
-        self.resultTable.setSizePolicy(sizePolicy1)
-        self.resultTable.setMinimumSize(QSize(0, 0))
-        self.resultTable.setLayoutDirection(Qt.LeftToRight)
-        self.resultTable.setAutoFillBackground(False)
-        self.resultTable.setFrameShadow(QFrame.Plain)
-        self.resultTable.setLineWidth(1)
-        self.resultTable.setSelectionMode(QAbstractItemView.NoSelection)
-        self.resultTable.setSelectionBehavior(QAbstractItemView.SelectItems)
-        self.resultTable.setTextElideMode(Qt.ElideLeft)
-        self.resultTable.setShowGrid(True)
-        self.resultTable.setGridStyle(Qt.NoPen)
-        self.resultTable.setWordWrap(True)
-        self.resultTable.setColumnCount(2)
-        self.resultTable.horizontalHeader().setVisible(True)
-        self.resultTable.horizontalHeader().setCascadingSectionResizes(True)
-        self.resultTable.horizontalHeader().setMinimumSectionSize(33)
-        self.resultTable.horizontalHeader().setDefaultSectionSize(300)
-        self.resultTable.verticalHeader().setVisible(True)
-        self.resultTable.verticalHeader().setMinimumSectionSize(20)
-        self.resultTable.verticalHeader().setDefaultSectionSize(30)
-
-        self.gridLayout.addWidget(self.resultTable, 1, 0, 1, 1)
-
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
@@ -122,14 +122,13 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        ___qtablewidgetitem = self.resultTable.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("MainWindow", u"Descriptor", None));
+        ___qtablewidgetitem1 = self.resultTable.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("MainWindow", u"Number of occurrences", None));
         self.medicalText.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Write medical text here...", None))
         self.searchButton.setText(QCoreApplication.translate("MainWindow", u"Search for descriptors", None))
         self.saveButton.setText(QCoreApplication.translate("MainWindow", u"Save results to text file", None))
         self.chartButton.setText(QCoreApplication.translate("MainWindow", u"Generate chart", None))
-        ___qtablewidgetitem = self.resultTable.horizontalHeaderItem(0)
-        ___qtablewidgetitem.setText(QCoreApplication.translate("MainWindow", u"Descriptor", None));
-        ___qtablewidgetitem1 = self.resultTable.horizontalHeaderItem(1)
-        ___qtablewidgetitem1.setText(QCoreApplication.translate("MainWindow", u"Number of occurrences\n"
-"", None));
     # retranslateUi
 
